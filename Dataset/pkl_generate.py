76d3f949d56b8eef5c3402f2720850d67d298853
QuantumScriptHub <161706167+QuantumScriptHub@users.noreply.github.com>
2025-12-03 21:51:51 +0800

    Initial commit

--- Dataset/pkl_generate.py
+++ Dataset/pkl_generate.py
@@ -3,8 +3,8 @@ import pickle
 
 
 data_dict = {}
-train_images = os.listdir('./COD10K-v3/Train/Image')
-test_images = os.listdir('./COD10K-v3/Test/Image')
+train_images = os.listdir('./R2C7K/Train/Image')
+test_images = os.listdir('./R2C7K/Test/Image')
 
 train_images = [x.removesuffix('.jpg') for x in train_images]
 data_dict['train'] = {}
@@ -13,6 +13,6 @@ test_images = [x.removesuffix('.jpg') for x in test_images]
 data_dict['test'] = {}
 data_dict['test']['name_list'] = test_images
 
-sav_path = 'cod10k_pickle.pkl'
+sav_path = 'R2C7K_pickle.pkl'
 with open(sav_path, 'wb') as file:
     pickle.dump(data_dict, file)
